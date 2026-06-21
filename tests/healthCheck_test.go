9ca33de4a936639f4339d39efbab58053bdbea4d
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2026-06-21 23:42:20 +0100

    improved health check test to check returned data

--- tests/healthCheck_test.go
+++ tests/healthCheck_test.go
@@ -3,6 +3,7 @@
 package tests
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,6 +11,10 @@ import (
 	"github.com/matheus-rib/golang-crud/config"
 	"github.com/stretchr/testify/assert"
 )
+
+type DataHealthCheckResponse struct {
+	Data string `json:"data"`
+}
 
 func Test_HealthCheckRoute(t *testing.T) {
 	router := config.SetupRouter()
@@ -21,5 +26,12 @@ func Test_HealthCheckRoute(t *testing.T) {
 		router.ServeHTTP(resp, req)
 
 		assert.Equal(t, http.StatusOK, resp.Code)
+
+		var response DataHealthCheckResponse
+		err := json.Unmarshal(resp.Body.Bytes(), &response)
+
+		assert.NoError(t, err, "Response body should be valid JSON")
+		assert.Equal(t, http.StatusOK, resp.Code)
+		assert.Equal(t, "alive", response.Data)
 	})
 }
